7b3350edde6d5c54460b304aa2ed42eda2ea65a7
thu <luuthikimthu2002@gmail.com>
2024-05-20 00:46:26 +0700

    frontend commit

---
+++ src/data.js
@@ -0,0 +1,80 @@
+import { formatter } from './formater';
+
+const itemName = [];
+
+export const items =[
+    {
+       image: require('./asset/images/featiem2.jpg'),
+       price: formatter('12000'),
+       id: 1,
+       title: itemName,
+       buy: 'Đã bán +101'
+    },
+    {
+       image: require('./asset/images/featiem2.jpg'),
+       price: formatter('12000'),
+       id: 1,
+       title: itemName,
+       buy: 'Đã bán +101'
+    },
+    {
+       image: require('./asset/images/featiem2.jpg'),
+       price: formatter('12000'),
+       id: 1,
+       title: itemName,
+       buy: 'Đã bán +101'
+    },
+    {
+        image: require('./asset/images/featiem2.jpg'),
+        price: formatter('12000'),
+        id: 1,
+        title: itemName,
+        buy: 'Đã bán +101'
+    },
+    {
+        image: require('./asset/images/featiem2.jpg'),
+        price: formatter('12000'),
+        id: 1,
+        title: itemName,
+        buy: 'Đã bán +101'
+    },        {
+        image: require('./asset/images/featiem2.jpg'),
+        price: formatter('12000'),
+        id: 1,
+        title: itemName,
+        buy: 'Đã bán +101'
+    },
+    {
+        image: require('./asset/images/featiem2.jpg'),
+        price: formatter('12000'),
+        id: 1,
+        title: itemName,
+        buy: 'Đã bán +101'
+    }, 
+    {
+        image: require('./asset/images/featiem2.jpg'),
+        price: formatter('12000'),
+        id: 1,
+        title: itemName,
+        buy: 'Đã bán +101'
+    }, 
+    {
+        image: require('./asset/images/featiem2.jpg'),
+        price: formatter('12000'),
+        id: 1,
+        title: itemName,
+        buy: 'Đã bán +101'
+    },
+    {
+       image: require('./asset/images/featiem2.jpg'),
+       price: formatter('12000'),
+       id: 1,
+       title: itemName,
+       buy: 'Đã bán +101'
+    }, 
+
+]
+
+export const cartItems = [
+  
+]
